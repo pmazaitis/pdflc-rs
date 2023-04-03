cd88468e9077aa424770c9cdd8f7893ae0e3f222
Paul Mazaitis <paul@mazaitis.org>
2023-04-03 15:43:09 +0000

    working with better framework, stdout writer

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,62 @@
 
 // pdflc-rs - simple PDF hyperlink reference checker
 
-
-
 extern crate pdf_extract;
-extern crate lopdf;
-// extern crate linkify;
 
+use std::io::{self, Write, BufWriter};
+use std::io::stdout;
+use std::fs::{File};
 use std::env;
 use std::path::PathBuf;
 use pdf_extract::extract_text;
 use linkify::{LinkFinder, LinkKind};
+use log::{ info, error, debug, warn };
+use reqwest::{StatusCode,Url};
 
-use clap::{arg, command, value_parser, ArgAction, Command};
+use clap::{arg, command, value_parser};
+// use clap::{ArgAction, Command};
 
+#[derive(Debug)]
+struct LinkStatus {
+    link: Url,
+    status: StatusCode
+}
 
+impl LinkStatus {
+    fn new(link: Url, status: StatusCode) -> Self {
+        LinkStatus{link, status}
+    }
+}
+
+fn test_links_in_pdf (input_path: &PathBuf) -> Result<Vec<LinkStatus>, String> {
+    
+    let mut tested_links: Vec<LinkStatus> = Vec::new();
+    
+    let pdf_text = extract_text(&input_path).unwrap();
+    
+    let mut finder = LinkFinder::new();
+    finder.kinds(&[LinkKind::Url]);
+    
+    let links: Vec<_> = finder.links(&pdf_text).collect();
+    
+    for link in links {
+        let slink = link.as_str();
+        let res = reqwest::blocking::get(slink).unwrap();
+        tested_links.push(LinkStatus::new(Url::parse(slink).unwrap(),res.status())) 
+    }
+    
+    Ok(tested_links)
+
+}
 
 fn main() {
-    // println!("Hello, world!");
+
+    env_logger::init();
+    
+    // error!("{}", "And error occurred");
+    // warn!("{:#?}", "This is important");
+    // info!("{:?}", "Take note");
+    // debug!("Something weird occurred: {}", "Error");
     
     let matches = command!() 
         .arg(arg!([input_path] "Path of PDF file to use")
@@ -28,22 +67,14 @@ fn main() {
 
     let input_path = matches.get_one::<PathBuf>("input_path").unwrap();
 
-    let pdf_text = extract_text(&input_path).unwrap();  
-    
-    let mut finder = LinkFinder::new();
-    
-    finder.kinds(&[LinkKind::Url]);
-    
-    let links: Vec<_> = finder.links(&pdf_text).collect();
- 
-    for link in links {
-        
-        let slink = link.as_str();
-        println!("Link: {:?}", &slink);
-        let res = reqwest::blocking::get(slink).unwrap();
-        println!("Status: {}", res.status());
-        
-    }
- 
-    // println!("{}",pdf_text);    
+
+    let stdout = io::stdout();
+    let mut write_handle = io::BufWriter::new(stdout);
+
+    let tested_links = test_links_in_pdf(input_path).unwrap();
+
+    for ls in tested_links.iter() {
+        writeln!(write_handle, "Link: {} - Status: {}", ls.link.as_str(), ls.status);
+    }    
+      
 }
