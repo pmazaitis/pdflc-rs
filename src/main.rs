589c3043c149764dca32db9ac565159d92b94e7a
Paul Mazaitis <paul@mazaitis.org>
2023-04-04 00:46:29 +0000

    building, but no file output

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 
 // pdflc-rs - simple PDF hyperlink reference checker
 
+    // error!("{}", "And error occurred");
+// warn!("{:#?}", "This is important");
+// info!("{:?}", "Take note");
+// debug!("Something weird occurred: {}", "Error");
+
 extern crate pdf_extract;
 
-use std::io::{self, Write, BufWriter};
+use std::io::{self, Write, BufWriter, Error};
 use std::io::stdout;
 use std::fs::{File};
 use std::env;
 use std::path::PathBuf;
 use pdf_extract::extract_text;
 use linkify::{LinkFinder, LinkKind};
-use log::{ info, error, debug, warn };
+// use log::{ info, error, debug, warn };
 use reqwest::{StatusCode,Url};
 
-use clap::{arg, command, value_parser};
+use clap::{arg, Arg, ArgMatches, command, value_parser};
 // use clap::{ArgAction, Command};
+
+// ///////////////////////////
+
+
 
 #[derive(Debug)]
 struct LinkStatus {
@@ -49,32 +58,49 @@ fn test_links_in_pdf (input_path: &PathBuf) -> Result<Vec<LinkStatus>, String> {
 
 }
 
+fn get_args() -> ArgMatches {
+    command!() 
+    .arg(arg!([input_path] "Path to PDF file")
+        .required(true)
+        .value_parser(value_parser!(PathBuf))
+    )
+    .arg(
+        arg!(
+            -o --output <FILE> "Output file"
+        )
+        .required(false)
+        .value_parser(value_parser!(PathBuf)),
+    )
+    .get_matches()
+}
+
 fn main() {
 
     env_logger::init();
     
-    // error!("{}", "And error occurred");
-    // warn!("{:#?}", "This is important");
-    // info!("{:?}", "Take note");
-    // debug!("Something weird occurred: {}", "Error");
-    
-    let matches = command!() 
-        .arg(arg!([input_path] "Path of PDF file to use")
-            .required(true)
-            .value_parser(value_parser!(PathBuf))
-        )
-        .get_matches();
+    let matches = get_args();
 
     let input_path = matches.get_one::<PathBuf>("input_path").unwrap();
-
 
     let stdout = io::stdout();
     let mut write_handle = io::BufWriter::new(stdout);
 
+    let write_handle_result: Result<Box<dyn Write>, Error> = match matches.get_one::<PathBuf>("output") {
+        Some(ref path) => File::open(path).map(|f| Box::new(f) as Box<dyn Write>),
+        None => Ok(Box::new(io::stdout())),
+    };
+
     let tested_links = test_links_in_pdf(input_path).unwrap();
 
-    for ls in tested_links.iter() {
-        writeln!(write_handle, "Link: {} - Status: {}", ls.link.as_str(), ls.status);
-    }    
+    if let Ok(mut write_handle) = write_handle_result {
+       for ls in tested_links.iter() {
+           writeln!(write_handle, "Link: {} - Status: {}", ls.link.as_str(), ls.status).expect("Unable to write.");
+       }
+       write_handle.flush().unwrap();  
+    };
+
+    
+
+   
       
 }
